50b4cdfab0352401a165a5360b7e4f425b9a916a
MrModadel <timanormu@gmail.com>
2023-01-22 16:44:45 +0500

    news

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let user_name = prompt('Как вас зовут?' );
 let user_age = prompt('Сколько вам лет?');
 
-if (user_name[0] === "a"||user_name[0] === "A" && user_age > 17) {
+if (user_name[0] === "a"&&user_age >= 18||user_name[0] === "A"&&user_age >= 18) {
    alert("Welcome to the site!")
 } else {
    alert ("Log out of the site")
-}
+};
