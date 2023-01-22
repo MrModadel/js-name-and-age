c684c893cde4b24de87ddfe009d52b213087b881
MrModadel <timanormu@gmail.com>
2023-01-22 16:21:11 +0500

    news

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+let user_name = prompt('Как вас зовут?' );
+let user_age = prompt('Сколько вам лет?');
+
+if (user_name.charAt(0) === "A" || user_name.charAt(0) === "a" === user_age > 17) {
+   alert("Welcome to the site!")
+} else {
+   alert ("Log out of the site")
+}
+
+
+
+
+
+
